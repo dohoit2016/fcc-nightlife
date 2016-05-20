7c6cd6c404b49ee390e36e0381de1d979161993b
Ngoc Don <kaitoukid2701@gmail.com>
2016-05-20 23:48:19 +0700

    Fix UI + heroku callback url

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'facebook': {
 		clientID: '267615070252084',
 		clientSecret: 'ad1b24984cec5bf529b31ada4c1c4936',
-		callbackURL: 'http://localhost:3000/auth/facebook/callback',
+		callbackURL: 'https://fcc-0127-nightlife.herokuapp.com/auth/facebook/callback',
 	}
 }
